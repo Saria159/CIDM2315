f42f5d233842138211f2f1c913bf134c8b6f9498
Saria159 <saria2@buffs.wtamu.edu>
2024-09-07 14:59:17 -0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Part 1
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4*X*X + 3*Y;
+        Console.WriteLine($"X = {X}, Y = {Y}");
+        Console.WriteLine($"The Value of Z is: {Z}");
+
+        //Part 2
+        
+    }
+}
