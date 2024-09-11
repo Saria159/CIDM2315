cd4203e73eb01065a482a1fa0fb3923485a09d01
Saria159 <saria2@buffs.wtamu.edu>
2024-09-11 09:14:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Week2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Please input your name: ");
+        string user_name = Console.ReadLine();
+        Console.WriteLine("Please input your age: ");
+        string user_age = Console.ReadLine();
+        //Convert user_age from string to int
+        int age_int = Convert.ToInt16(user_age);
+        int birth_year = 2024 - age_int;
+        Console.WriteLine($"{user_name} was born in {birth_year}");
+
+        int num1 = 10;
+        int num2 = 20;
+        Console.WriteLine(num1>num2); // 10>20 false
+        Console.WriteLine(num1<num2); // 10<20 true
+        Console.WriteLine(num1!=num2); // 10 != 20 true
+
+        int x=3, y=10;
+        if(x<y)
+        {
+            Console.WriteLine($"x = {x}, y = {y}");
+            Console.WriteLine($"x<y: {x<y}");
+        }
+        Console.WriteLine("Code below if statement");
+        
+        string user_name = "alice";
+        Console.WriteLine("Please input username: ");
+        string input_username = Console.ReadLine();
+        if(input_username == user_name){
+            Console.WriteLine("Login Successful");
+        }
+        else{
+            Console.WriteLine("Login failed, wrong username");
+        }
+        
+        int c=30, d=10;
+        Console.WriteLine($"c = {c}, d={d}");
+        // if-else statement
+        if(c>=d){
+            Console.WriteLine($"if-block, c>=d; {c>=d}");
+        }
+        else{
+            Console.WriteLine($"else-block, c>=d: {c>=d}");
+        }
+        
+  Console.WriteLine("Please Enter a number:");
+        string x = Console.ReadLine();
+        int int_x = Convert.ToInt16(x);
+        if(int_x >= 0){
+            Console.WriteLine($"{int_x} is positive");
+        }
+        else{
+            Console.WriteLine($"{int_x} is negative");
+        }
+
+        if(int_x%2 == 0){
+            Console.WriteLine($"{int_x} is even");
+        }
+        else{
+            Console.WriteLine($"{int_x} is odd");
+        }
+        
+
+        Console.WriteLine("Please Enter a number:");
+        string x = Console.ReadLine();
+        int int_x = Convert.ToInt16(x);
+        if(int_x > 0){
+            Console.WriteLine($"{int_x} is positive");
+        }
+        else if (int_x < 0){
+             Console.WriteLine($"{int_x} is negative");
+        }
+        else {
+             Console.WriteLine($"{int_x} is 0");
+        }
+
+        */
+
+        
+    }
+}
