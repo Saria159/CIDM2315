c8ae181bd27cb73277b61c97e997e705505c25e8
Saria159 <saria2@buffs.wtamu.edu>
2024-09-11 09:15:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Week1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       /* int a = 10;
+        int b = a;
+        b = b+20; // 10+20 = b
+        Console.WriteLine(b);
+        
+        int my_age = 29;
+        string my_name = "Alice";
+        Console.WriteLine($"My name is {my_name}, I am {my_age} years old.");
+        
+
+        int Year = 2022;
+        string Major = "CIS";
+        float stu_gpa = 3.5f;
+        double Weight = 73.5;
+        char letter_grade = 'A';
+        bool is_online = true;
+        
+
+        const int LabCapacity = 50; // const can not change
+        Console.WriteLine(LabCapacity);
+        //LabCapacity = 30; // error
+        
+
+        int a = 10;
+        int b = 3;
+        double c = 2.4;
+        Console.WriteLine(a/b);
+        Console.WriteLine(a/c);
+        Console.WriteLine(a*b);
+        Console.WriteLine(a*c);
+        Console.WriteLine(a-c);
+        Console.WriteLine(a%b);
+        Console.WriteLine((a-b).GetType().Name);
+        
+
+        int a = 1;
+        int b = ++a; // ++a: a = a+1 = 1+1 = 2; b = 2;
+        Console.WriteLine($"a = {a}"); // a = 2
+        Console.WriteLine($"b = {b}"); // b = 2
+        */
+        int a = 1;
+        int b = a--;
+        Console.WriteLine($"a = {a}"); // a = 2
+        Console.WriteLine($"b = {b}"); // b = 2
+    }
+}
